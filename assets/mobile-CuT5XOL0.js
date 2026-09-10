d887de203728476bc394896584e2e3c45fa760e0
Ebrahim El Harrak <ebrahimeht57@gmail.com>
2026-09-10 21:56:48 +0200

    Updates

--- assets/mobile-CuT5XOL0.js
+++ assets/mobile-CuT5XOL0.js
@@ -1,4 +1,4 @@
-import{W as yi,a as hr}from"./worlds-BN2L0X8L.js";function xi(o,e){for(var n=0;n<e.length;n++){var t=e[n];t.enumerable=t.enumerable||!1,t.configurable=!0,"value"in t&&(t.writable=!0),Object.defineProperty(o,t.key,t)}}function bi(o,e,n){return e&&xi(o.prototype,e),o}/*!
+import{W as yi,a as hr}from"./worlds-UQhgLOon.js";function xi(o,e){for(var n=0;n<e.length;n++){var t=e[n];t.enumerable=t.enumerable||!1,t.configurable=!0,"value"in t&&(t.writable=!0),Object.defineProperty(o,t.key,t)}}function bi(o,e,n){return e&&xi(o.prototype,e),o}/*!
  * Observer 3.15.0
  * https://gsap.com
  *
