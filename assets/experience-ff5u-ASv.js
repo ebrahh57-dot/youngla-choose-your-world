0422011f9ed549e9b9bddb79a50cc0886c0827b5
Ebrahim El Harrak <ebrahimeht57@gmail.com>
2026-09-11 11:09:39 +0200

    Updates

--- assets/experience-ff5u-ASv.js
+++ assets/experience-ff5u-ASv.js
@@ -1,4 +1,4 @@
-import{W as Ri,a as ue}from"./worlds-D32P4HSO.js";import{s as la}from"./main-CZg3G8fa.js";/**
+import{W as Ri,a as ue}from"./worlds-Cn_RxRkr.js";import{s as la}from"./main-H-AtiGdK.js";/**
  * @license
  * Copyright 2010-2024 Three.js Authors
  * SPDX-License-Identifier: MIT
